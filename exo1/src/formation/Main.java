144920cdd9888f13c3971ec310820fb2c14704f8
stagiaire <belhadj.karim@hotmail.fr>
2017-08-24 10:10:40 +0200

    jai ajouté un commentaire

--- exo1/src/formation/Main.java
+++ exo1/src/formation/Main.java
@@ -4,6 +4,11 @@ import junit.framework.*;
 
 public class Main {
 
+  /*
+   *  un commentaire
+   * 
+   * */	
+	
   public static Test suite() {
     TestSuite suite = new TestSuite("Tous les tests");
     suite.addTestSuite(AllTests.class);
